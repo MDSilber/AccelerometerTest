c6f72765ae6bca59f1b0b2d229ef753be03855cb
Mason Silber <mason@twitter.com>
2014-06-11 11:46:44 -0400

    Removed unused code

--- AccelerometerTest/ViewController.m
+++ AccelerometerTest/ViewController.m
@@ -6,14 +6,13 @@
 //  Copyright (c) 2014 MasonSilber. All rights reserved.
 //
 
-#import "FluidView.h"
 #import "ViewController.h"
 #import <CoreMotion/CoreMotion.h>
 
 @interface ViewController ()
 @property (nonatomic) CMMotionManager *motionManager;
 @property (nonatomic) UIView *block;
-@property (nonatomic) FluidView *fluidView;
+@property (nonatomic) UIView *fluidView;
 
 @property (nonatomic) UIGravityBehavior *gravityBehavior;
 @property (nonatomic) UIAttachmentBehavior *attachmentBehavior;
@@ -36,7 +35,8 @@
     self.block.clipsToBounds = YES;
     [self.view addSubview:self.block];
 
-    self.fluidView = [[FluidView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 300.0f, 300.0f)];
+    self.fluidView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 300.0f, 300.0f)];
+    self.fluidView.backgroundColor = [UIColor colorWithRed:212.0f/255.0f green:151.0f/255.0f blue:76.0f/255.0f alpha:1.0f];
     self.fluidView.center = CGPointMake(50.0f, 200.0f);
     [self.block addSubview:self.fluidView];
 
@@ -58,26 +58,13 @@
         __weak typeof(self) weakSelf = self;
         [self.motionManager startAccelerometerUpdatesToQueue:[NSOperationQueue mainQueue] withHandler:^(CMAccelerometerData *accelerometerData, NSError *error) {
             typeof(self) strongSelf = weakSelf;
-            NSLog(@"%f, %f", accelerometerData.acceleration.x, accelerometerData.acceleration.y);
-
-
+//            NSLog(@"%f, %f", accelerometerData.acceleration.x, accelerometerData.acceleration.y);
 
             //                        x: 0.0f, y: -1.0f
             //     x: -1.0f, y: 0.0f                     x: 1.0f, y: 0.0f
             //                        x: 0.0f, y: 1.0f
 
-//            strongSelf.gravityBehavior.gravityDirection = CGVectorMake(10.0f * accelerometerData.acceleration.x, -10.0f * accelerometerData.acceleration.y);
-
-            // Moves the center block
-            //CGPoint center = strongSelf.block.center;
-            //center.x = MIN(MAX(center.x + accelerometerData.acceleration.x * 10.0f, 50.0f), CGRectGetWidth(self.view.bounds) - 50.0f);
-            //center.y = MIN(MAX(center.y - accelerometerData.acceleration.y * 10.0f, 50.0f), CGRectGetHeight(self.view.bounds) - 50.0f);
-            //strongSelf.block.center = center;
-            
-//            strongSelf.fluidView.xAcceleration = accelerometerData.acceleration.x;
-//            strongSelf.fluidView.yAcceleration = accelerometerData.acceleration.y;
-//            [strongSelf.fluidView setNeedsDisplay];
-
+            // Factor of 10 is to pull down on liquid much more strongly than horizontal forces
             strongSelf.gravityBehavior.gravityDirection = CGVectorMake(accelerometerData.acceleration.x, 10 * fabsf(accelerometerData.acceleration.y));
         }];
     }
