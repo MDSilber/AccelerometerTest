673a4743fe33f8fbdd510d9f99a9306527ad3fd1
Mason Silber <mason@twitter.com>
2014-08-27 21:30:28 -0400

    Added ice cubes and stuff

--- AccelerometerTest/ViewController.m
+++ AccelerometerTest/ViewController.m
@@ -11,12 +11,11 @@
 
 @interface ViewController ()
 @property (nonatomic) CMMotionManager *motionManager;
-@property (nonatomic) UIView *containerView;
-@property (nonatomic) UIView *fluidView;
-
-@property (nonatomic) UIGravityBehavior *gravityBehavior;
+@property (nonatomic) UIView *containerView, *fluidView, *iceCube1, *iceCube2;
+@property (nonatomic) UIGravityBehavior *fluidGravityBehavior, *iceCubeGravityBehavior;
 @property (nonatomic) UIAttachmentBehavior *attachmentBehavior;
-@property (nonatomic) UIDynamicItemBehavior *itemBehavior;
+@property (nonatomic) UIDynamicItemBehavior *fluidItemBehavior;
+@property (nonatomic) UICollisionBehavior *iceCubeCollisionBehavior;
 @property (nonatomic) UIDynamicAnimator *animator;
 @end
 
@@ -40,16 +39,40 @@
     self.fluidView.center = CGPointMake(50.0f, 200.0f);
     [self.containerView addSubview:self.fluidView];
 
+
+    self.iceCube1 = [[UIView alloc] initWithFrame:CGRectMake(12.0f, 74.0f, 25.0f, 25.0f)];
+    self.iceCube1.backgroundColor = [UIColor clearColor];
+    self.iceCube1.layer.cornerRadius = 6.0f;
+    self.iceCube1.layer.borderColor = [UIColor blackColor].CGColor;
+    self.iceCube1.layer.borderWidth = 2.0f;
+    [self.containerView addSubview:self.iceCube1];
+
+    CGRect iceCubeFrame = self.iceCube1.frame;
+    iceCubeFrame.origin.y -= CGRectGetHeight(self.iceCube1.bounds);
+    iceCubeFrame.origin.x += 12.0f;
+    self.iceCube2 =  [[UIView alloc] initWithFrame:iceCubeFrame];
+    self.iceCube2.backgroundColor = [UIColor clearColor];
+    self.iceCube2.layer.cornerRadius = 6.0f;
+    self.iceCube2.layer.borderColor = [UIColor blackColor].CGColor;
+    self.iceCube2.layer.borderWidth = 2.0f;
+    [self.containerView addSubview:self.iceCube2];
+
     self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.containerView];
 
-    self.gravityBehavior = [[UIGravityBehavior alloc] initWithItems:@[self.fluidView]];
+    self.fluidGravityBehavior = [[UIGravityBehavior alloc] initWithItems:@[self.fluidView]];
+
+    self.iceCubeGravityBehavior = [[UIGravityBehavior alloc] initWithItems:@[self.iceCube1, self.iceCube2]];
 
     self.attachmentBehavior = [[UIAttachmentBehavior alloc] initWithItem:self.fluidView offsetFromCenter:UIOffsetMake(0.0f, -1.0f * CGRectGetHeight(self.fluidView.frame) / 2.0f) attachedToAnchor:CGPointMake(CGRectGetWidth(self.containerView.frame) / 2.0f, (CGRectGetHeight(self.containerView.frame) / 2.0f) - 10.0f)];
     self.attachmentBehavior.length = 10.0f;
-    self.attachmentBehavior.damping = 0.5f;
+    self.attachmentBehavior.damping = 0.0f;
 
-    self.itemBehavior = [[UIDynamicItemBehavior alloc] initWithItems:@[self.fluidView]];
-    self.itemBehavior.angularResistance = 10.0f;
+    self.fluidItemBehavior = [[UIDynamicItemBehavior alloc] initWithItems:@[self.fluidView]];
+    self.fluidItemBehavior.angularResistance = 10.0f;
+
+    self.iceCubeCollisionBehavior = [[UICollisionBehavior alloc] initWithItems:@[self.iceCube1, self.iceCube2]];
+    [self.iceCubeCollisionBehavior addBoundaryWithIdentifier:@"container" forPath:[UIBezierPath bezierPathWithRect:self.containerView.bounds]];
+    [self.animator addBehavior:self.iceCubeCollisionBehavior];
 
     [self.animator addBehavior:self.attachmentBehavior];
 
@@ -65,7 +88,8 @@
             //                        x: 0.0f, y: 1.0f
 
             // Factor of 10 is to pull down on liquid much more strongly than horizontal forces
-            strongSelf.gravityBehavior.gravityDirection = CGVectorMake(accelerometerData.acceleration.x, 10 * fabsf(accelerometerData.acceleration.y));
+            strongSelf.fluidGravityBehavior.gravityDirection = CGVectorMake(accelerometerData.acceleration.x, 10 * fabsf(accelerometerData.acceleration.y));
+            strongSelf.iceCubeGravityBehavior.gravityDirection = CGVectorMake(accelerometerData.acceleration.x, fabsf(accelerometerData.acceleration.y));
         }];
     }
 }
@@ -74,8 +98,10 @@
 {
     [super viewDidAppear:animated];
 
-    self.gravityBehavior.gravityDirection = CGVectorMake(0.0f, -1.0f);
-    [self.animator addBehavior:self.gravityBehavior];
+    self.fluidGravityBehavior.gravityDirection = CGVectorMake(0.0f, 1.0f);
+    self.iceCubeGravityBehavior.gravityDirection = CGVectorMake(0.0f, 1.0f);
+    [self.animator addBehavior:self.fluidGravityBehavior];
+    [self.animator addBehavior:self.iceCubeGravityBehavior];
 }
 
 @end
